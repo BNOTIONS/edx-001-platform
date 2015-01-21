480c31533359e6f8965a10b1303a6e0d47f9e9a9
Daniel Eidan <Daniel.Eidan@gmail.com>
2015-01-20 20:31:36 -0500

    implement API endpoint for getting friends that are in a specific course

--- lms/djangoapps/mobile_api/friends/urls.py
+++ lms/djangoapps/mobile_api/friends/urls.py
@@ -2,12 +2,15 @@
 URLs for friends API
 """
 from django.conf.urls import patterns, url
+from django.conf import settings
+
 from .views import FriendsInCourse
 
 urlpatterns = patterns(
     'mobile_api.course_info.views',
     url(
-        r'^course/(?P<course_id>([\w]+/?)+)$',
+        # r'^course/(?P<course_id>(([\w]|\.)+/?)+)$', #TODO: remove this
+        r'^course/{}$'.format(settings.COURSE_ID_PATTERN),
         FriendsInCourse.as_view(),
         name='friends-in-course'
         ),
--- lms/djangoapps/mobile_api/friends/views.py
+++ lms/djangoapps/mobile_api/friends/views.py
@@ -5,8 +5,32 @@ Views for friends info API
 from rest_framework import generics, permissions
 from rest_framework.authentication import OAuth2Authentication, SessionAuthentication
 from rest_framework.response import Response
-
+from student.models import CourseEnrollment
+from courseware import models
+from enrollment import api
+from third_party_auth import pipeline, provider
 from nose.tools import set_trace
+from openedx.core.djangoapps.user_api.models import User, UserProfile, UserPreference, UserOrgTag
+from opaque_keys.edx.locator import CourseLocator
+from opaque_keys.edx.locations import SlashSeparatedCourseKey
+from social.apps.django_app.default.models import UserSocialAuth
+
+from facebook_data_mocker import FacebookDataMocker
+
+
+
+# TODO: dependencies to be added to the vagrant 
+import facebook
+
+# TODO: This should not be in the final commit
+_APP_SECRET = "8a982cfdc0922c9fe57bd63edab6b62f"
+_APP_ID = "734266930001243"
+
+#TODO: pass this in 
+# _USER_ACCESS_TOKEN = 'CAACEdEose0cBADF45nBgdDKVDGkcivBtSUuE1BCtL9DPH3uZARTucemNNpBjVeTz0B25eOMakCTSdKL53DeSyd0mYm1lNSfRhwGadN9Va1LG6B2VoaSdZATGiC8eDaoJsGsDucmwwesHERSMyVUpyjKlxK1smVvmcZA0GneKTzgn15DZCyqzZCrv2jg28cSNZBKYRUmgj6JTfW2GIh5KU8'
+
+_FACEBOOK_API_VERSION = "v2.2/"
+
 
 class FriendsInCourse(generics.ListAPIView):
     """
@@ -27,6 +51,7 @@ class FriendsInCourse(generics.ListAPIView):
                 [{
                     "name": "test",
                     "id": "12345",
+                    "edX_id": "678910"
                 },
                 ...
                 ]
@@ -36,15 +61,54 @@ class FriendsInCourse(generics.ListAPIView):
     permission_classes = (permissions.IsAuthenticated,)
 
     def list(self, request, *args, **kwargs):
-        return Response(
-            {"friends": [{  "name": "Daniel Eidan",
-                            "id": "10154831816670300"},
-                        {   "name": "Marc Ashman", 
-                            "id": "10154833899435243"},
-                        {   "name": "Peter Organa", 
-                            "id": "10154805420820176"}
-                        ]
-            }
-        )
+        # set_trace()
+        # get all the users FB friends
+        oauth_token = self.get_token(request)
+        if oauth_token:
+            graph = facebook.GraphAPI(oauth_token)
+            url = _FACEBOOK_API_VERSION + "me/friends"
+            friends = graph.request(url)
+            # TODO: deal with pagination
+                    
+            # For each friend check if they are a linked edX user
+            friends_that_are_edX_users = []
+            for friend in friends['data']:
+                name = friend['name']
+                fb_id = friend['id']
+                query_set = UserSocialAuth.objects.filter(uid=unicode(fb_id))
+                if query_set.count() == 1: 
+                    friend['edX_id'] = query_set[0].user_id
+                    friends_that_are_edX_users.append(friend)
 
+
+            if 'course_id' in kwargs:
+                course_path = kwargs['course_id'].split('/') 
+                if len(course_path) == 3: 
+                    ss_course_key = SlashSeparatedCourseKey(course_path[0], course_path[1], course_path[2])
+                else:
+                    #TODO: assert that this never happens   
+                    pass
+            else: 
+                #TODO: assert that this never happens 
+                pass
+            
+            # For each edX friend check if they are a member of that course 
+            # and if so add them to the result set
+            fb_friends_in_course = []
+            for friend in friends_that_are_edX_users:
+                query_set = CourseEnrollment.objects.filter(course_id = ss_course_key, 
+                                                            user_id =  friend['edX_id'] )
+                if query_set.count() == 1:
+                    fb_friends_in_course.append(friend)
+
+
+            # TODO: filter based on TOC after merging with TOC branch
+
+            fb_friends_in_course = {'friends' : fb_friends_in_course}
+            return Response(fb_friends_in_course)
+        return Response({})
+
+    def get_token(self, request): 
+        if 'oauth-token' in request.GET:
+            return request.GET['oauth-token']
 
